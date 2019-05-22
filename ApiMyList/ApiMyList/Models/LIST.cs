4761cacae76e6a0ba0a385e13d4c60748107d9e6
David <davidvalencia_@hotmail.com>
2019-05-22 10:18:52 +0200

    Comienzo la API

---
+++ ApiMyList/ApiMyList/Models/LIST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMyList.Models
+{
+    [Table("PRODUCTLIST")]
+    public class LIST
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("NOMBRE")]
+        public String Nombre { get; set; }
+        [Column("DESCRIPCION")]
+        public String Descripcion { get; set; }
+        [Column("FECHA")]
+        public DateTime Fecha { get; set; }
+        [Column("PRESUPUESTO")]
+        public float Presupuesto { get; set; }
+        [Column("ACTIVARLIMITE")]
+        public bool ActivarLimite { get; set; }
+        [Column("PRESUPUESTOLIMITE")]
+        public float PresupuestoLimite { get; set; }
+        [Column("MEDIAPERSONA")]
+        public float MediaPersona { get; set; }
+    }
+}
---
+++ ApiMyList/ApiMyList/Models/PRODUCT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMyList.Models
+{
+    [Table("PRODUCT")]
+    public class PRODUCT
+    {
+        [Key]
+        [Column("ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        [Column("IDLISTA")]
+        public int IdLista { get; set; }
+        [Column("NOMBRE")]
+        public String Nombre { get; set; }
+        [Column("CANTIDAD")]
+        public int Cantidad { get; set; }
+        [Column("PRECIO")]
+        public float Precio { get; set; }
+        [Column("CHECKED")]
+        public bool Checked { get; set; }
+    }
+}
